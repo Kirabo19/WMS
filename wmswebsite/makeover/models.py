6b25bac0e9f2723bc69ea5c417e7b71d884d76fc
Kirabo19 <kapojames@gmail.com>
2022-01-26 04:27:25 +0300

    DB Configuration

--- wmswebsite/makeover/models.py
+++ wmswebsite/makeover/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class makeover(models.Model):
+    website = models.CharField(max_length=250)
+    description = models.TextField()
+    keyword = models.TextField()
+    i_email = models.EmailField()
+    fb = models.CharField(max_length=250)
+    tw = models.CharField(max_length=250)
+    yb = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.website
+
