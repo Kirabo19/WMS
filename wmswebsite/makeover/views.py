c685fb4302b13597d11018f251f192eb13ed6d18
Kirabo19 <kapojames@gmail.com>
2022-01-28 12:12:44 +0300

    Changes

--- wmswebsite/makeover/views.py
+++ wmswebsite/makeover/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from models import makeover
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'makeover/index.html')
+    update_make = makeover.objects.all()
     
+    return render(request, 'makeover/index.html') 
 
 def add_system(request):
     return render(request, 'makeover/add_system.html')
