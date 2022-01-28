6a6d368029ea71ce1b0fcde5fc7910c020a79624
Kirabo19 <kapojames@gmail.com>
2022-01-28 12:19:05 +0300

    Changes

--- wmswebsite/makeover/views.py
+++ wmswebsite/makeover/views.py
@@ -4,9 +4,11 @@ from models import makeover
 
 # Create your views here.
 def index(request):
-    update_make = makeover.objects.all()
-    
-    return render(request, 'makeover/index.html') 
+    update_makeover = makeover.objects.all()
+    context = {
+        'update_makeover':update_makeover
+    }
+    return render(request, 'makeover/index.html', update_makeover) 
 
 def add_system(request):
     return render(request, 'makeover/add_system.html')
