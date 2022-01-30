d36450609a995d4e82a99cf264605b2798c6ef74
Kirabo19 <kapojames@gmail.com>
2022-01-30 19:53:25 +0300

    Changes

--- wmswebsite/admin_staff/views.py
+++ wmswebsite/admin_staff/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#from .models import admin_staff
+#from django.contrib import messages
 
 # Create your views here.
+def index(request):
+     return render(request, 'admin_staff/index.html') 
+     
--- wmswebsite/wmswebsite/urls.py
+++ wmswebsite/wmswebsite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('makeover.urls')),
+    path('',include('admin_staff.urls')),
     path('admin/', admin.site.urls),
 ]
