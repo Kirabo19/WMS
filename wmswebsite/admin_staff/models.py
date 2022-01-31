735d7b8baa2121f27b69fe612be0e27e5e9ccf25
Kirabo19 <kapojames@gmail.com>
2022-01-31 09:49:14 +0300

    Changes

--- wmswebsite/admin_staff/models.py
+++ wmswebsite/admin_staff/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class admin_staff(models.Model):
+    admin_user = models.CharField(max_length=225)
+    admin_password = models.CharField(max_length=225)
+    admin_name =models.CharField(max_length=225)
+    admin_address = models.CharField(max_length=225)
+    admin_phone = models.CharField(max_length=225)
+    admin_group = models.CharField(max_length=225)
+    admin_date = models.DateField(default=now)
