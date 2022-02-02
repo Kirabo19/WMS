c0b3f594b83f405e7baea23d12c62d6075fbd226
Kirabo19 <kapojames@gmail.com>
2022-02-02 05:46:47 +0300

    Started new app supp

--- wmswebsite/supplier/models.py
+++ wmswebsite/supplier/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.utils.timezone import now
 
 # Create your models here.
+class supplier(models.Model):
+    supplier_name = models.CharField(max_length=225)
+    supplier_address = models.CharField(max_length=225)
+    supplier_phone = models.CharField(max_length=225)
+    supplier_created = models.DateField(default=now)
+    supplier_updated = models.DateField(default=now)
+
+def __str__(self):
+        return self.supplier_name 
--- wmswebsite/supplier/views.py
+++ wmswebsite/supplier/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import supplier
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
     return render(request, 'supplier/index.html')
 
+
 def add_supplier(request):
-    return render(request, 'supplier/addsupplier.html')
+    if request.method == 'GET':
+       return render(request, 'supplier/addsupplier.html')
+
+    if request.method =='POST':
+       supplier_name = request.POST['supplier_name']
+       supplier_address = request.POST['supplier_address']
+       supplier_phone = request.POST['supplier_phone']
+       #supplier_created = request.POST['supplier_created']
+       #supplier_updated = request.POST['supplier_updated']   
+
+    supplier.objects.create(supplier_name=supplier_name, supplier_address=supplier_address, supplier_phone=supplier_phone)
+    messages.success(request, 'New supplier has been added successfully!,')
+    return redirect('supplier')
