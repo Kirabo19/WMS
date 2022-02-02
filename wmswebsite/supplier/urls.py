31dc8323e0c15fe7fb1911835315a18135e04404
Kirabo19 <kapojames@gmail.com>
2022-02-02 11:59:30 +0300

    Started new app supp

--- wmswebsite/supplier/urls.py
+++ wmswebsite/supplier/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name="supplier"),
-    path('add_supplier',views.add_supplier, name="add_supplier")
+    path('add_supplier',views.add_supplier, name="add_supplier"),
+    path('edit_supplier',views.edit_supplier, name="edit_supplier")
   
 ]
--- wmswebsite/supplier/views.py
+++ wmswebsite/supplier/views.py
@@ -5,9 +5,16 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'supplier/index.html')
+     suppliers = supplier.objects.all()
+     supps = supplier.objects.count()
+     context = {
+          'suppliers': suppliers, 
+          'supps': supps   
+     }
 
+     return render(request, 'supplier/index.html', context)
 
+# Create function 
 def add_supplier(request):
     if request.method == 'GET':
        return render(request, 'supplier/addsupplier.html')
@@ -22,3 +29,19 @@ def add_supplier(request):
     supplier.objects.create(supplier_name=supplier_name, supplier_address=supplier_address, supplier_phone=supplier_phone)
     messages.success(request, 'New supplier has been added successfully!,')
     return redirect('supplier')
+
+# Edit function 
+def edit_supplier(request, id):
+
+    edit_supps = supplier.objects.all()
+    context = {
+        'edit_supps': edit_supps
+    }
+    if request.method == 'GET':
+       return render(request,'supplier/editsupplier.html', context)
+
+    if request.method == 'POST': 
+       supplier_name = request.POST['supplier_name']
+       supplier_address = request.POST['supplier_address']
+       supplier_phone = request.POST['supplier_phone']
+
